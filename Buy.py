a51d5ecd8492b5f79709acf0644db87a73497cdf
HridoY Ahammed Nirob <102291823+Cyber-Nirob-Official@users.noreply.github.com>
2022-07-25 12:27:26 +0600

    Add files via upload

---
+++ Buy.py
@@ -0,0 +1,373 @@
+import os
+import sys
+import time
+import requests
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+print(""" 
+
+This Is Target I'd Hack Working Command
+You Are NoT Premium User
+Buy Subscription From Cyber Nirob
+
+
+
+""")
+os.system('xdg-open https://www.facebook.com/The.Cyber.BD.Hacker')
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m'
+O = '\x1b[1;96m'
+green="\033[0;32m"
+bigreen="\033[1;92m"
+bired="\033[1;91m"
+biyellow="\033[1;93m"
+biblue="\033[1;94m"
+bipurple="\033[1;95m"
+bicyan="\033[1;96m"
